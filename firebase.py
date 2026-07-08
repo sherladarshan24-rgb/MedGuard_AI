67807a88585ec090d6f4931e3675c1b8df4d5ef7
Sherla Darshan <sherladarshan24@gmail.com>
2026-07-08 11:34:17 +0530

    Configure Firebase for Render

--- firebase.py
+++ firebase.py
@@ -1,16 +1,27 @@
+import os
+import json
 import firebase_admin
-from firebase_admin import credentials, firestore
 
-# Initialize Firebase only once
+from firebase_admin import credentials, firestore
+from dotenv import load_dotenv
+
+load_dotenv()
+
 if not firebase_admin._apps:
-    cred = credentials.Certificate("serviceAccountKey.json")
+
+    firebase_config = json.loads(
+        os.environ["FIREBASE_CREDENTIALS"]
+    )
+
+    cred = credentials.Certificate(firebase_config)
+
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 
 
-# Get all patients from Firestore
 def get_patients():
+
     docs = db.collection("patients").stream()
 
     patients = []
